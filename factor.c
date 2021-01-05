8f5c82440a8b8e9fac40c1e86aa6a25a02e774dc
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-01-05 08:59:31 -0600

    Moved the includes. Fixed indent. factor works.

--- factor.c
+++ factor.c
@@ -1,20 +1,20 @@
-int main() {
-  /*#include <math.h>*/
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-float answ;
-int fin, i, entry;
-div_t test;
-puts("Enter an integer to factor.");
-scanf("%d",&entry);
-answ=sqrt(entry);
-fin=floor(answ);
-for (i=1;i<=fin;i++)
-{
-test=div(entry,i);
-if (test.rem==0)
-printf("%d * %d\n",i,test.quot);
-}
-return 0;
-}
 
+int main() {
+  float answ;
+  int fin, i, entry;
+  div_t test;
+  puts("Enter an integer to factor.");
+  scanf("%d",&entry);
+  answ=sqrt(entry);
+  fin=floor(answ);
+  for (i=1;i<=fin;i++)
+    {
+      test=div(entry,i);
+      if (test.rem==0)
+	printf("%d * %d\n",i,test.quot);
+    }
+  return 0;
+}
