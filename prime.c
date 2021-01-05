a2be3eb1c019b8231d166a58f3ec06ce7b6b0d1b
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-01-05 09:07:16 -0600

    prime.c improve style

--- prime.c
+++ prime.c
@@ -3,32 +3,36 @@
 #include <stdio.h>
 
 int main(){
-float p;
-int a,b,r,z,found,prime;
-div_t x;
-puts("Enter range for list of primes as follows: start,end");
-scanf("%d,%d",&a,&b);
-for(z=a;z<=b;z++)
-{
-p=sqrt(z);
-r=1;
-found=0;
-prime=0;
-do
-      {
-      r=r+1;
-      x=div(z,r);
-      if(x.rem==0)
-      found=1;
-      if(r>p)
-             {
-             found=1;
-             prime=1;
-             }
-      }
-    while (found==0);
-if (prime==1)
-printf("%d ",z);
-}
-return 0;
+  float p;
+  int a, b, r, z, found, prime;
+  div_t x;
+
+  puts("Enter range for list of primes as follows: start,end");
+  scanf("%d,%d", &a, &b);
+
+  for(z=a; z<=b; z++)
+    {
+      p = sqrt(z);
+      r = 1;
+      found = 0;
+      prime = 0;
+      do
+	{
+	  r = r + 1;
+	  x = div(z, r);
+	  if(x.rem == 0)
+	    found = 1;
+	  if(r > p)
+	    {
+	      found = 1;
+	      prime = 1;
+	    }
+	}
+      while (found == 0);
+
+      if (prime == 1)
+	printf("%d ", z);
+    }
+  printf("\n");
+  return 0;
 }
